e7fc4e527857dd153f212d1775970a0218e11fd2
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-10-04 00:02:27 +0100

    Deploy SAR backend and update website

---
+++ AWS_SAR.tf
@@ -0,0 +1,16 @@
+resource "aws_serverlessapplicationrepository_cloudformation_stack" "serverless-form-handler" {
+  name           = "APIGW-Lambda-DynamoDB"
+  application_id = "arn:aws:serverlessrepo:us-east-1:780018668030:applications/serverless-form-handler"
+
+  capabilities = [
+    "CAPABILITY_IAM",
+    "CAPABILITY_RESOURCE_POLICY",
+  ]
+  parameters = {
+    # functionName = "func-postgres-rotator"
+    ValidatedEmail     = "christopher.ogbunuzor@cloudreach.com"
+  }
+}
+
+data "aws_partition" "current" {}
+data "aws_region" "current" {}
