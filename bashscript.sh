d94d58a3e583cbefdf1ff7676d22ff5d366d262f
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-10-06 08:30:21 +0100

    update bashscript with cloudwatch logs name

--- bashscript.sh
+++ bashscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws logs describe-log-streams --log-group-name chrisDockerLogGroup --region eu-west-1 | jq > ./CWLogStreams.json
-aws logs filter-log-events --log-group-name chrisDockerLogGroup --start-time `date -d 2022-10-01T14:50:00Z`000 --region eu-west-1 | jq > ./CWLogEvents.json
-aws logs filter-log-events --log-group-name chrisDockerLogGroup --start-time `date -d 2022-10-01T14:50:00Z`000 --region eu-west-1 | jq .events[].message > ./CWLogMessages.txt
+aws logs describe-log-streams --log-group-name slaveserverDockerLogGroup --region eu-west-1 | jq > ./CWLogStreams.json
+aws logs filter-log-events --log-group-name slaveserverDockerLogGroup --start-time `date -d 2022-10-01T14:50:00Z`000 --region eu-west-1 | jq > ./CWLogEvents.json
+aws logs filter-log-events --log-group-name slaveserverDockerLogGroup --start-time `date -d 2022-10-01T14:50:00Z`000 --region eu-west-1 | jq .events[].message > ./CWLogMessages.txt
 
