28d4c8c525ba4dbd58ee177ddbab1a9cf796070b
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-10-06 08:28:42 +0100

    update bashscript to display log messages in a text file

--- bashscript.sh
+++ bashscript.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-aws logs describe-log-streams --log-group-name chrisDockerLogGroup --region eu-west-1 | jq > ./CWLogStreams.txt
+aws logs describe-log-streams --log-group-name chrisDockerLogGroup --region eu-west-1 | jq > ./CWLogStreams.json
+aws logs filter-log-events --log-group-name chrisDockerLogGroup --start-time `date -d 2022-10-01T14:50:00Z`000 --region eu-west-1 | jq > ./CWLogEvents.json
+aws logs filter-log-events --log-group-name chrisDockerLogGroup --start-time `date -d 2022-10-01T14:50:00Z`000 --region eu-west-1 | jq .events[].message > ./CWLogMessages.txt
+
